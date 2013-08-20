8a9f3ae661e86cf7e23f6f283216aaea867d1870
cphoover <cphoover@gmail.com>
2013-08-20 17:14:00 -0400

    Updated to work with yuglify

--- multiLine.js
+++ multiLine.js
@@ -1,4 +1,4 @@
 Function.prototype.multiLine = function () {
-    for (var b = this.toString().split("\n"), c = null, d = null, a = 0; a < b.length; a++) /\/\*\~/.test(b[a]) ? c = a + 1 : /~\*\//g.test(b[a]) && (d = a);
+    for (var b = this.toString().split("\n"), c = null, d = null, a = 0; a < b.length; a++) /\/\*\!/.test(b[a]) ? c = a + 1 : /\*\//g.test(b[a]) && (d = a);
     return b.slice(c, d).join("\n")
 };
