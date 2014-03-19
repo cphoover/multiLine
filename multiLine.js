d7c11b2790aacfb7704858a0e33ba85c5a41c0b3
cphoover <cphoover@gmail.com>
2014-03-19 13:32:28 -0400

    Update multiLine.js
    No need to modify function prototype.
    
    using regular expressions instead

--- multiLine.js
+++ multiLine.js
@@ -1,4 +1,1 @@
-Function.prototype.multiLine = function () {
-    for (var b = this.toString().split("\n"), c = null, d = null, a = 0; a < b.length; a++) /\/\*\!/.test(b[a]) ? c = a + 1 : /\*\//g.test(b[a]) && (d = a);
-    return b.slice(c, d).join("\n")
-};
+function multiLine(a){a=/function.*\(\)\w*{.*\/\*\!?([\w\s]*)(?=\*\/)/.exec(a.toString());return null===a?!1:a[1]};
